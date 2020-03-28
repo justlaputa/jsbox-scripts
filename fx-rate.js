bd2051be491518ca6072a8ac7a5f8a30f5122ebe
Xiao Han <justlaputa@gmail.com>
2020-03-28 19:11:05 +0900

    add more rates

--- fx-rate.js
+++ fx-rate.js
@@ -1,4 +1,6 @@
-var names = ["🇺🇸USD ↔ 🇯🇵JPY", "🇿🇦ZAR ↔ 🇯🇵JPY"];
+var names = ["🇺🇸USD ↔ 🇯🇵JPY", "🇿🇦ZAR ↔ 🇯🇵JPY", '🇯🇵JPY ↔ 🇨🇳CNY', "HNS ↔ ₿BTC"];
+var rates = {};
+var namebaseAuth = ""
 
 $ui.render({
   props: { title: "" },
@@ -41,15 +43,6 @@ $ui.render({
       events: {
         pulled: function() {
           fetch(true);
-        },
-        didSelect: function(sender, indexPath) {
-          var base = rates[symbols[selectedIndex]] || 1.0;
-          var number = Number($("input").text);
-          $clipboard.text = (
-            (number * (rates[symbols[indexPath.row]] || 1.0)) /
-            base
-          ).toFixed(4);
-          $ui.toast($l10n("copied"));
         }
       }
     }
@@ -69,35 +62,113 @@ function updateList() {
       "value-label": {
         text: 0.1
       }
+    },
+    {
+      "name-label": { text: names[2] },
+      "value-label": {
+        text: 0.1
+      }
+    },
+    {
+      "name-label": { text: names[3] },
+      "value-label": {
+        text: 0.1
+      }
     }
   ];
-  var lines = rates.split("\n");
-  for (let line of lines) {
-    var fields = line.split(",");
-    if (fields[0] === "USDJPY") {
-      ratesData[0]["value-label"].text = `${fields[1]},${fields[2]}`;
-    } else if (fields[0] === "ZARJPY") {
-      ratesData[1]["value-label"].text = `${fields[1]},${fields[2]}`;
+
+  let symbols = ['USDJPY', 'ZARJPY', 'JPYCNY', 'HSNBTC'];
+
+  for (let i = 0; i < symbols.length; i++) {
+    if (rates[symbols[i]]) {
+      let symbolData = rates[symbols[i]];
+      ratesData[i]["value-label"].text = symbolData[0] + "," + symbolData[1];
     }
   }
 
   $("list").data = ratesData;
 }
 
-function fetch(pulled) {
+async function fetch(pulled) {
   $ui.loading(!pulled);
-  $http.get({
-    url: "https://info.fcd.japannetbank.co.jp/csv//rate.php?" + Date.now(),
-    handler: function(resp) {
-      if (resp.response.statusCode >= 300) {
-        return;
+  var result = await Promise.all([
+    $http.get({
+      url: "https://info.fcd.japannetbank.co.jp/csv/rate.php?" + Date.now()
+    }),
+    $http.get({
+      url: "https://www.namebase.io/api/v0/ticker/day?symbol=HNSBTC",
+      header: {
+        'Authorization': namebaseAuth
       }
-      $ui.loading(false);
-      $("list").endRefreshing();
-      rates = resp.data;
-      updateList();
-    }
-  });
+    }),
+    $http.get({
+      url: 'https://srh.bankofchina.com/search/whpj/search_cn.jsp?erectDate=&nothing=&pjname=%E6%97%A5%E5%85%83&head=head_620.js&bottom=bottom_591.js&page=1'
+    })
+  ]);
+
+  if (result[0].response.statusCode < 300) {
+    console.log('got result from japannet', result[0].data);
+    let fxRates = processCSV(result[0].data);
+    rates = Object.assign({}, fxRates);
+  }
+
+  if (result[1].response.statusCode < 300) {
+    console.log('got result from namebase', result[1].data);
+    rates["HSNBTC"] = [result[1].data.closePrice, `${result[1].data.priceChangePercent}%`];
+  } else {
+    console.error(result[1].response);
+  }
+
+  if (result[2].response.statusCode < 300) {
+    console.log('got response from boc site', result[2].data.len)
+    let jpyRates = processBocHTML(result[2].data)
+    rates['JPYCNY'] = jpyRates;
+  }
+
+  $ui.loading(false);
+  $("list").endRefreshing();
+  updateList();
 }
 
-fetch(false);
+function processCSV(data) {
+  let result = {};
+  let lines = data.split("\n");
+  for (let line of lines) {
+    var fields = line.split(",");
+    if (fields[0] === "USDJPY" || fields[0] === "ZARJPY") {
+      result[fields[0]] = [fields[1], fields[2]];
+    }
+  }
+  return result;
+}
+
+function processBocHTML(data) {
+  let doc = $xml.parse({
+    string: data,
+    mode: 'html'
+  });
+
+  let buy = doc.rootElement.firstChild({
+    xPath: '//div[@class="BOC_main publish"]/table/tr[2]/td[2]/text()'
+  });
+  
+  let middle = doc.rootElement.firstChild({
+    xPath: '//div[@class="BOC_main publish"]/table/tr[2]/td[6]/text()'
+  });
+
+  let result = [0, 0]
+
+  if (buy && buy.node) {
+    result[0] = buy.node;
+  }
+  if (middle && middle.node) {
+    result[1] = middle.node;
+  }
+
+  return result;
+}
+
+fetch(false).then(
+  () => console.log('done'),
+  e => console.error(e)
+);
